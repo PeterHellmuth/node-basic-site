d99a1252f17ac91f5bfb15205973630a67ef196f
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-10-23 10:20:30 -0500

    Setup basic 3 page http server

---
+++ index.js
@@ -0,0 +1,30 @@
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+
+const hostname = "localhost";
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.writeHead(200, { "Content-Type": "text/html" });
+
+  let serveHTML = "404.html";
+  const pathPairs = [
+    { path: "/", file: "index.html" },
+    { path: "/about", file: "about.html" },
+    { path: "/contact-me", file: "contact-me.html" },
+  ];
+
+  pathPairs.forEach((pair) => {
+    if (req.url === pair.path) {
+      serveHTML = pair.file;
+    }
+  });
+
+  fs.createReadStream(path.resolve(__dirname, serveHTML)).pipe(res);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
